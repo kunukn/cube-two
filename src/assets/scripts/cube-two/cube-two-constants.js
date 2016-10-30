2e67d94348ff41760b562d1d7b9ef4303f302397
Kunuk Nykjær <kunukn@gmail.com>
2016-10-30 12:01:10 +0100

    rework button layout

--- src/assets/scripts/cube-two/cube-two-constants.js
+++ src/assets/scripts/cube-two/cube-two-constants.js
@@ -1,3 +1,2 @@
 export const CUBE_COUNT = 8;
-export const CUBE_SIZE = '22vmin';
-export const CUBE_SIZE_HALF = '11vmin'; /* remember to also edit _variables.scss */
+export const CUBE_SIZE_HALF = '12.5vmin'; /* remember to also edit _variables.scss */
