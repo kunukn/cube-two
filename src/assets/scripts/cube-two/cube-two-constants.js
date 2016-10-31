0c30e87fc717b82863a04401127523093c5777b0
Kunuk Nykjær <kunukn@gmail.com>
2016-10-31 22:42:52 +0100

    make rotation look good on safari

--- src/assets/scripts/cube-two/cube-two-constants.js
+++ src/assets/scripts/cube-two/cube-two-constants.js
@@ -1,2 +1,3 @@
 export const CUBE_COUNT = 8;
-export const CUBE_SIZE = '25vmin'; /* remember to also edit _variables.scss */
+//export const CUBE_SIZE = '25vmin'; /* remember to also edit _variables.scss */
+export const CUBE_SIZE = '0'; /* remember to also edit _variables.scss */
