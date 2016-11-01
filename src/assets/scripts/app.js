2ac47f0590d88bf93398c25d30236e01abecaa74
Kunuk Nykjær <kunukn@gmail.com>
2016-11-01 22:40:47 +0100

    add solve and scramble to ui

--- src/assets/scripts/app.js
+++ src/assets/scripts/app.js
@@ -45,12 +45,16 @@ function checkForComplete() {
 // const imgPathFox =  '/src/assets/images/fox.svg';
 const imgPathFox = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/157670/fox.svg';
 
-const cubeTwo = new CubeTwo({
+const cubetwo = new CubeTwo({
     cubeComponent: cubeComponentEl,
-    isTapEnabled: true, // default: true
-    isRotateAnimationEnabled: true, // default: true
-    transition: 'transform cubic-bezier(0.4, 0.0, 0.2, 1) 200ms', // default: transform 200ms    
-    backgroundImages: {
+    /* required */
+    isTapEnabled: true,
+    /* default: true */
+    isRotateAnimationEnabled: true,
+    /* default: true */
+    transition: 'transform cubic-bezier(0.4, 0.0, 0.2, 1) 200ms',
+    /* default: transform 200ms */
+    backgroundImages: { /* optional */
         u: imgPathFox,
         d: imgPathFox,
         f: imgPathFox,
@@ -58,7 +62,7 @@ const cubeTwo = new CubeTwo({
         r: imgPathFox,
         l: imgPathFox,
     },
-    backgroundColors: {
+    backgroundColors: { /* optional */
         u: 'rgba(255, 255, 255, .9)',
         f: 'rgba(0, 158, 96, .9)',
         r: 'rgba(196, 30, 58, .9)',
@@ -68,23 +72,29 @@ const cubeTwo = new CubeTwo({
         backface: 'transparent',
     },
 });
-cubeTwo.addCallbackForEvent('init', initCallback);
-cubeTwo.addCallbackForEvent('statechange', statechangeCallback);
-cubeTwo.addCallbackForEvent('beforerotate', beforerotateCallback);
-cubeTwo.addCallbackForEvent('afterrotate', afterrotateCallback);
-cubeTwo.init();
+cubetwo.addCallbackForEvent('init', initCallback);
+cubetwo.addCallbackForEvent('statechange', statechangeCallback);
+cubetwo.addCallbackForEvent('beforerotate', beforerotateCallback);
+cubetwo.addCallbackForEvent('afterrotate', afterrotateCallback);
+cubetwo.init();
 
 
 /*
     Setup UI buttons
 */
 const cubetwoBtnMenuEl = qs('.cubetwo-js.cubetwo-btn-menu', cubeComponentEl),
-    cubetwoMenuEl = qs('.cubetwo-menu-component');
+    cubetwoMenuEl = qs('.cubetwo-menu-component'),
+    cubetwoSolve = qs('.cubetwo-btn-solve', cubetwoMenuEl),
+    cubetwoScramble = qs('.cubetwo-btn-scramble', cubetwoMenuEl);
 
 cubetwoBtnMenuEl.addEventListener('click', ev => {
     cubetwoMenuEl.classList.toggle('cubetwo-show-dialog');
     ev.currentTarget.classList.toggle('cubetwo-active');
 });
+
+cubetwoSolve.addEventListener('click', _ => cubetwo.solve());
+cubetwoScramble.addEventListener('click', _ => cubetwo.scramble());
+
 
 const cubetwoBtnHelpEl = qs('.cubetwo-js.cubetwo-btn-help', cubeComponentEl),
     cubetwoHelpEl = qs('.cubetwo-help-component');
@@ -107,28 +117,28 @@ qs('.cubetwo-btn-bottom-right', cubeComponentEl).addEventListener('click',
     ev => cubetwoRotationViewEl.style.transform = `rotateX(-${ROTATION_VIEW.X}deg) rotateY(${ROTATION_VIEW.Y}deg) rotateZ(0deg)`
 );
 qs('.cubetwo-btn-top-center', cubeComponentEl).addEventListener('click',
-    ev => cubeTwo.x()
+    ev => cubetwo.x()
 );
 qs('.cubetwo-btn-bottom-center', cubeComponentEl).addEventListener('click',
-    ev => cubeTwo.x_()
+    ev => cubetwo.x_()
 );
 qs('.cubetwo-js.cubetwo-btn-rotate-left', cubeComponentEl).addEventListener('click',
-    ev => cubeTwo.y_()
+    ev => cubetwo.y_()
 );
 qs('.cubetwo-js.cubetwo-btn-rotate-left-2x', cubeComponentEl).addEventListener('click',
-    ev => cubeTwo.y2_()
+    ev => cubetwo.y2_()
 );
 qs('.cubetwo-js.cubetwo-btn-rotate-right', cubeComponentEl).addEventListener('click',
-    ev => cubeTwo.y()
+    ev => cubetwo.y()
 );
 qs('.cubetwo-js.cubetwo-btn-rotate-right-2x', cubeComponentEl).addEventListener('click',
-    ev => cubeTwo.y2()
+    ev => cubetwo.y2()
 );
-window.addEventListener('keydown', cubeTwo.handleGlobalKeyEvent, false);
+window.addEventListener('keydown', cubetwo.handleGlobalKeyEvent, false);
 
 /*
     Expose as Library to window object
 */
-window.cubetwo = cubeTwo;
+window.cubetwo = cubetwo;
 
 log('cubetwo is available in console');
