2259683b8b1b985b36dc26d706cba3d5406194e6
Kunuk Nykjær <kunukn@gmail.com>
2016-11-01 15:18:49 +0100

    add B and B_ reducer

--- src/assets/scripts/cube-two/cube-two-reducer.js
+++ src/assets/scripts/cube-two/cube-two-reducer.js
@@ -16,17 +16,20 @@ import dictCubeTransform from '../dictionaries/dict-cube-transform';
 
 export function reducer({ action, stateCodes }) {
 
+    function applyHelper(c1, c2, c3, c4, actionCode) {
+        c1.code = dictCube[c1.code][actionCode];
+        c2.code = dictCube[c2.code][actionCode];
+        c3.code = dictCube[c3.code][actionCode];
+        c4.code = dictCube[c4.code][actionCode];
+    }
+
     function applyF() {
         let c1 = stateCodes[0];
         let c2 = stateCodes[1];
         let c3 = stateCodes[2];
         let c4 = stateCodes[3];
-        const actionCode = 'z';
 
-        c1.code = dictCube[c1.code][actionCode];
-        c2.code = dictCube[c2.code][actionCode];
-        c3.code = dictCube[c3.code][actionCode];
-        c4.code = dictCube[c4.code][actionCode];
+        applyHelper(c1, c2, c3, c4, 'z');
 
         let temp = c1.cube;
         c1.cube = c3.cube;
@@ -40,12 +43,38 @@ export function reducer({ action, stateCodes }) {
         let c2 = stateCodes[1];
         let c3 = stateCodes[2];
         let c4 = stateCodes[3];
-        const actionCode = '-z';
 
-        c1.code = dictCube[c1.code][actionCode];
-        c2.code = dictCube[c2.code][actionCode];
-        c3.code = dictCube[c3.code][actionCode];
-        c4.code = dictCube[c4.code][actionCode];
+        applyHelper(c1, c2, c3, c4, '-z');
+
+        let temp = c1.cube;
+        c1.cube = c2.cube;
+        c2.cube = c4.cube;
+        c4.cube = c3.cube;
+        c3.cube = temp;
+    }
+
+    function applyB_() {
+        let c1 = stateCodes[4];
+        let c2 = stateCodes[5];
+        let c3 = stateCodes[6];
+        let c4 = stateCodes[7];
+
+        applyHelper(c1, c2, c3, c4, 'z');
+
+        let temp = c1.cube;
+        c1.cube = c3.cube;
+        c3.cube = c4.cube;
+        c4.cube = c2.cube;
+        c2.cube = temp;
+    }
+
+    function applyB() {
+        let c1 = stateCodes[4];
+        let c2 = stateCodes[5];
+        let c3 = stateCodes[6];
+        let c4 = stateCodes[7];
+
+        applyHelper(c1, c2, c3, c4, '-z');
 
         let temp = c1.cube;
         c1.cube = c2.cube;
@@ -55,14 +84,21 @@ export function reducer({ action, stateCodes }) {
     }
 
 
-    if (action === 'F') {
-        applyF();
+    switch (action) {
+        case 'F':
+            applyF();
+            break;
+        case 'F_':
+            applyF_();
+            break;
+        case 'B':
+            applyB();
+            break;
+        case 'B_':
+            applyB_();
+            break;
     }
-    if (action === 'F_') {
-        applyF_();
-    }
-
-
+    
     //log(JSON.stringify(stateCodes));
     return stateCodes;
 }
